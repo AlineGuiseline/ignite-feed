8c04027f2cd01f240ba1806576100c6e93abe602
Aline Guiseline <alineguiseline@gmail.com>
2025-03-18 16:45:46 -0300

    first configs + creates sidebar

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Aline Guiseline"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti officiis voluptatibus eius excepturi eum repellat magni veritatis amet sed quas, ipsum, rerum iure, quae aspernatur quam architecto harum? Delectus, dicta."
+          />
+          <Post author="Bia Fernandes" content="Um post super legal!" />
+        </main>
+      </div>
+    </>
+  );
+}
+
+/* 
+nós temos o arquivo index.html dentro deste projeto e ele é o primeiro arquivo carregado pela aplicação web. Neste index nós temos apenas uma div vazia (id="root") e um script do tipo "module" (lembrando que o Vite utiliza dos ECMAScripts modules nativos do browser, ou seja, o fomrato de importação e exportação que existe nativamente nos novos browsers) que informa que o arquivo .js que a nossa aplicação vai carregar é o main.jsx
+
+Mas como tem tanta coisa sendo exibida na tela se a div root está vazia?
+
+Se nós entrarmos no arquivo main.jsx nós vemos que ele está importando o React, o "react-dom/client". O "react" é o core da biblioteca, onde tem as funcionalidades que são compartilhadas entre todas as interfaces, seja React para web, React Native, React para a televisão... Já o "react-dom/client" é a integração do core do React com a DOM (Document Object Model, ou seja, a representação do HTML através do JS), então quando nós improtamos o react dom, nós integramos o react para funcionar no ambiente web do browser. 
+
+Nós chamamos o método .createRoot() de dentro do ReactDOM, e ele recebe um parâmetro, que é o elemento raiz da nossa página HTML (no caso, o "root"), e o React vai criar o HTML, o CSS e todo o JS da aplicação dentro da div root com o método .render(). O render vai renderizar algo interno do React ("React.StrictMode") e dentro desse "React.StrictMode" será renderizado o nosso App (que nada mais é do que uma função que retorna um HTML).
+
+Ou seja, diferente de uma aplicação tradicional, a aplicação SPA terá toda a interface construída a partir do JS (a interface não fica dentro do HTML, mas sim do JS, o que torna mais fácil de manipulá-la).
+*/
